d652a24862d277fb14511448f505560af460b332
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-27 22:11:44 -0300

    feat: asChild Link, buttons as link

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,33 @@
 import { Link } from "expo-router"
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
-export default function Index() {
+export default function Signup() {
   return (
     <View style={styles.container}>
-      <Link href="./sign-up" style={styles.new}>
-        Criar conta
+      <Text style={styles.title}>Início</Text>
+      <Link href={'./sign-up'} asChild>
+        <TouchableOpacity style={styles.button} >
+          <Text style={styles.label}>
+            sign-up
+          </Text>
+        </TouchableOpacity>
       </Link>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  new: { fontSize: 16, fontWeight: "bold" },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: { fontSize: 22, fontWeight: "bold" },
+  label: { fontSize: 16, fontWeight: "bold", color: "#FFF" },
+  button: {
+    backgroundColor: "#000",
+    paddingHorizontal: 32,
+    paddingVertical: 10,
+    borderRadius: 10
+  }
 })
