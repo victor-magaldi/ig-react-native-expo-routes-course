207768ab3556dfc4f602446d021f24dd558f71c3
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-27 22:02:55 -0300

    feat: implement link return home

--- src/app/sign-up.tsx
+++ src/app/sign-up.tsx
@@ -1,14 +1,22 @@
+import { Link } from "expo-router"
 import { StyleSheet, Text, View } from "react-native"
 
 export default function Signup() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Conta</Text>
+      <Link href={'./'}>
+        Voltar
+      </Link>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   title: { fontSize: 22, fontWeight: "bold" },
 })
