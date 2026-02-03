bafd42a0662a3c4c48e5158fbf5d03a02892a410
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-02-02 21:48:46 -0300

    feat: init tabs

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,18 @@
+// import { Slot } from "expo-router"
+
+import { Tabs } from "expo-router"
+
+// import { Stack } from "expo-router"
+export default function Layout() {
+  return (
+    // <View style={{ flex: 1 }}>
+    //   <Slot />
+    //   <View style={{ backgroundColor: "#d5d5d5", height: 50, width: "100%" }}></View>
+    // </View>
+    // <Stack screenOptions={{}}>
+    //   <Stack.Screen name="index" options={{ title: 'Sign In' }} />
+    //   <Stack.Screen name="sign-up" options={{ title: 'Cadastrar' }} />
+    // </Stack>
+    <Tabs screenOptions={{ tabBarActiveBackgroundColor: "#d5d5d5" }} />
+  )
+}
